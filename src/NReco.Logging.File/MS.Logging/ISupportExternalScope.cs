732d8316a24e0b379b1937bda071ab233708fb24
aalbert <aalbert@tipconsulting.io>
2020-12-19 23:24:34 -0700

    Add MS logging classes that do not exist in logging v1 used by this project.  These objects simpliy the creation and management of scopes. We cannot upgrade to the latest .net logging version for compatibility reasons, so we are manually patching in these classes

---
+++ src/NReco.Logging.File/MS.Logging/ISupportExternalScope.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Nreco.Logging.File.Microsoft.Extensions.Logging
+{
+    /// <summary>
+    /// Represents a <see cref="ILoggerProvider"/> that is able to consume external scope information.
+    /// </summary>
+    internal interface ISupportExternalScope
+    {
+        /// <summary>
+        /// Sets external scope information source for logger provider.
+        /// </summary>
+        /// <param name="scopeProvider"></param>
+        void SetScopeProvider(IExternalScopeProvider scopeProvider);
+    }
+}
