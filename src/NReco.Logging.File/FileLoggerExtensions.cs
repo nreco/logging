1e9b9911e9cc650667247ac89ef02b41caef3e7b
Vitalii Fedorchenko <fedorchenko@gmail.com>
2023-12-24 10:11:59 +0200

    NReco's FileLoggerExtensions should NOT be in the Microsoft.Extensions.Logging namespace. #61

--- src/NReco.Logging.File/FileLoggerExtensions.cs
+++ src/NReco.Logging.File/FileLoggerExtensions.cs
@@ -16,14 +16,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using NReco.Logging.File;
 
-namespace Microsoft.Extensions.Logging
-{
+namespace NReco.Logging.File {
 
     public static class FileLoggerExtensions
     {
