64b61e32c6206210ffaf0cafc7efacc98bcb0b2d
Ryan Esteves <snargledorf@users.noreply.github.com>
2024-09-19 13:09:20 -0400

    Fixed generic types for AddSingleton
    Removed the FileLoggerProvider TImplementation so that NullLoggerProvider can be returned by the implementation factory method.

--- src/NReco.Logging.File/FileLoggerExtensions.cs
+++ src/NReco.Logging.File/FileLoggerExtensions.cs
@@ -74,7 +74,7 @@ namespace NReco.Logging.File {
 		/// </summary>
 		/// <remarks>File logger is not added if "File" section is not present or it doesn't contain "Path" property.</remarks>
 		public static ILoggingBuilder AddFile(this ILoggingBuilder builder, Action<FileLoggerOptions> configure = null) {
-			builder.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>(
+			builder.Services.AddSingleton<ILoggerProvider>(
 				(srvPrv) => {
 					IConfigurationSection loggingSection = srvPrv.GetRequiredService<IConfiguration>().GetSection("Logging");
 					return CreateFromConfiguration(loggingSection, configure) ?? NullLoggerProvider.Instance;
